2ec51375d6a9fc70e6833adffddae4bd11415f0a
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2023-01-06 13:20:35 -0500

    HTTPS Checker, checks url if user is on the developer version of our website!

---
+++ resources/htaccess.js
@@ -0,0 +1,13 @@
+function checkUrl(w, b) {
+  this.w = window;
+  this.b = w.location.href;
+  if (b.includes("bichive.github.io")) {
+    alert("Dev: testing-website...");
+    r = prompt("What is your status?");
+    if (prompt = 'bichive') {
+      alert("Well; then welcome to the developer version of our website!");
+    }
+  }
+}
+
+checkUrl();
